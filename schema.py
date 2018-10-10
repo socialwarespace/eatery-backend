e2f87909fd8a3da4d51c32d4cd93ff55860893be
Conner Swenberg <cls364@cornell.edu>
2018-10-10 16:48:50 -0400

    Initialize files

---
+++ schema.py
@@ -0,0 +1,8 @@
+from graphene import Field, ObjectType, String, List, Int, Float, Boolean
+from graphene.types.datetime import Date, Time
+
+class Data(object):
+  dining_halls = {}
+  menus = {}
+  items = {}
+
