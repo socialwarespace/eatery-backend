b097fd52f0cf7d9acacd4ba0963019db4344cb3d
Conner Swenberg <cls364@cornell.edu>
2018-11-07 14:38:27 -0500

    Alphabetize constants

--- constants.py
+++ constants.py
@@ -1,5 +1,3 @@
-GET_URL = 'https://services.get.cbord.com/GETServices/services/json'
-
 ACCOUNT_NAMES = {
     'brbs': 'BRB Big Red Bucks',
     'citybucks': 'CB1 City Bucks',
@@ -7,14 +5,9 @@ ACCOUNT_NAMES = {
     'laundry': 'LAU Sem Laundry'
     # 'swipes': TBD
 }
-
-CORNELL_INSTITUTION_ID = '73116ae4-22ad-4c71-8ffd-11ba015407b1'
-
-UPDATE_DELAY = 86400  # 24 hours in seconds
-UPDATE_DELAY_TESTING = 60  # 1 minute in seconds
-ASSET_BASE_URL = 'https://raw.githubusercontent.com/cuappdev/assets/master/eatery/'
-STATIC_EATERIES_URL = 'https://raw.githubusercontent.com/cuappdev/DiningStack/master/DiningStack/externalEateries.json'
 CORNELL_DINING_URL = 'https://now.dining.cornell.edu/api/1.0/dining/eateries.json'
+CORNELL_INSTITUTION_ID = '73116ae4-22ad-4c71-8ffd-11ba015407b1'
+GET_URL = 'https://services.get.cbord.com/GETServices/services/json'
 GIT_CONTENT_URL = 'https://raw.githubusercontent.com/cuappdev'
 IMAGES_URL = GIT_CONTENT_URL + '/assets/master/eatery/eatery-images/'
 NUM_DAYS_STORED_IN_DB = 8
